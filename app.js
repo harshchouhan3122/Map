4a84cce960d2f9f6398f2760f22b57c92520b773
harshchouhan3122 <harshchouhan3121@gmail.com>
2024-11-13 13:04:25 +0530

    Searching with coordinates

--- app.js
+++ app.js
@@ -2,36 +2,26 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// For ejs connectivity
+// Set up EJS and static files
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
-// Serve static files like CSS
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Root Directory
-app.get("/", (req, res) => {res.send(" New Root Directory");});
+// Route for home
+app.get("/", (req, res) => { res.send("New Root Directory"); });
 
-// Dummy Map
-app.get("/showMap", (req, res) => {
-    res.render("map.ejs");
-});
-
+// Render the map with default coordinates if none are provided
 app.get('/map', (req, res) => {
-    const latitude = req.query.lat || 28.5204 ;
-    const longitude = req.query.lng || 79.8567 ;
-    // const latitude = req.query.lat ;
-    // const longitude = req.query.lng ;
-
-
-    // Log details to ensure values are passed correctly
+    const latitude = req.query.lat || 28.644800;  // Default to Delhi
+    const longitude = req.query.lng || 77.216721;
     console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
-
-    res.render('map.ejs', { latitude, longitude });
+    res.render('map', { latitude, longitude });
 });
-
 
 // Start server
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
+
+
+// GeoCoding
